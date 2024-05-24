4e6a2f1e6d83f914bbd66885da0874c2dc40a135
ncoldwave <robbi.mobile@gmail.com>
2024-05-24 17:00:38 +0200

    Adding Spring Framework project files

---
+++ src/main/java/hu/obuda/nik/springvertexaigemini/SpringVertexAiGeminiApplication.java
@@ -0,0 +1,13 @@
+package hu.obuda.nik.springvertexaigemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVertexAiGeminiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVertexAiGeminiApplication.class, args);
+    }
+
+}
