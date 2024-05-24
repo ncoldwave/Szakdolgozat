fa349458cb028fcf28d67dab3fd8c8a36d724afc
ncoldwave <robbi.mobile@gmail.com>
2024-05-24 18:03:20 +0200

    Adding Spring Actuator capabilities

---
+++ src/main/java/hu/obuda/nik/springvertexaigemini/MemoryHealthIndicator.java
@@ -0,0 +1,29 @@
+package hu.obuda.nik.springvertexaigemini;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemoryHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        double freeMemoryPercent = ((double) freeMemory / (double) totalMemory);
+
+        if (freeMemoryPercent > 25) {
+            return Health.up()
+                    .withDetail("freeMemory", freeMemory + " bytes")
+                    .withDetail("totalMemory", totalMemory + " bytes")
+                    .withDetail("freeMemoryPercent", totalMemory + " bytes")
+                    .build();
+        } else {
+            return Health.down()
+                    .withDetail("freeMemory", freeMemory + " bytes")
+                    .withDetail("totalMemory", totalMemory + " bytes")
+                    .withDetail("freeMemoryPercent", totalMemory + " bytes")
+                    .build();
+        }
+    }
