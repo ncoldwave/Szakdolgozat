b7c573e44caccf5cc3493924e3b6e9f6a5ea8d3c
ncoldwave <robbi.mobile@gmail.com>
2024-05-24 18:09:01 +0200

    Adding empty test and modify readme.md

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Szakdolgozat c. tantárgy kódmelléklete
 
 Tesztelés:
 vicc generálás: localhost:8080/ai/generate
+saját kérdés feltétele UI-n keresztül localhost:8080/index.html
---
+++ src/test/java/hu/obuda/nik/springvertexaigemini/SpringVertexAiGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.obuda.nik.springvertexaigemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVertexAiGeminiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
